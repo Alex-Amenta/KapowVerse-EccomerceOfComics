98f7420a64ceaecdf0bcbc68b41133989e578e87
Alex-Amenta <thekinggcat@gmail.com>
2023-10-11 23:14:48 -0300

    slice y pagination

--- Client/src/components/pagination/Pagination.jsx
+++ Client/src/components/pagination/Pagination.jsx
@@ -1,9 +1,38 @@
-import usePagination from "../../hooks/usePagination";
+import PaginationMaterial from "@mui/material/Pagination";
+import styles from "./Pagination.module.css";
+import { styled } from "@mui/system";
+import { Link } from "react-router-dom";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
-const Pagination = () => {
-  const { currentPage, totalPages, currentItems, paginate } =
-    usePagination(data);
-  return <></>;
+const Pagination = ({ totalPages, currentPage, onPageChange }) => {
+  return (
+    <div className={styles.container}>
+      {currentPage !== 1 && (
+        <Link
+          to="#"
+          className={styles.Link}
+          onClick={() => onPageChange(currentPage - 1)}
+          title="Página anterior"
+        >
+          <ArrowLeftIcon />
+        </Link>
+      )}
+      <span className={styles.pageIndicator}>
+        {currentPage} de {totalPages}
+      </span>
+      {currentPage !== totalPages && (
+        <Link
+          to="#"
+          className={styles.button}
+          onClick={() => onPageChange(currentPage + 1)}
+          title="Página siguiente"
+        >
+          <ArrowRightIcon />
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Pagination;
--- Client/src/views/home/Home.jsx
+++ Client/src/views/home/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../components/navbar/Navbar";
 import { useEffect } from "react";
 import { fetchComics } from "../../redux/features/comicSlice";
 import CardsContainer from "../../components/cards-container/CardsContainer";
+import Pagination from "../../components/pagination/Pagination";
+import usePagination from "../../hooks/usePagination";
 
 function Home() {
   const dispatch = useDispatch();
@@ -12,10 +14,18 @@ function Home() {
     dispatch(fetchComics());
   }, []);
 
+  const { currentPage, totalPages, currentItems, paginate } =
+    usePagination(allComics);
+
   return (
     <>
       <Navbar />
-      <CardsContainer allComics={allComics} />
+      <CardsContainer allComics={currentItems} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={paginate}
+      />
     </>
   );
 }
