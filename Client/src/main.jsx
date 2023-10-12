579bf0175adb5b374e9049329c4caf4fcc0b5f34
Fabrizio Rondinella <fabrondinella@gmail.com>
2023-10-12 15:53:43 -0300

    deploy-2

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,17 +1,33 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+import LandingPage from "./views/landing/LandingPage";
+import Home from "./views/home/Home";
+
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <LandingPage />,
+	},
+	{
+		path: "/home",
+		element: <Home />,
+	},
+	// {
+	// 	path: "/detail/:param",
+	// 	element: <Detail />,
+	// },
+	// {
+	// 	path: "/add",
+	// 	element: <AddForm />,
+	// }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<Provider store={store}>
+		<RouterProvider router={router}></RouterProvider>
+	</Provider>
 );
