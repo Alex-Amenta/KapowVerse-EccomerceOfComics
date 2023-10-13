abc907fade16977982b1138e62fc1a5552a7e87c
Fabrizio Rondinella <fabrondinella@gmail.com>
2023-10-13 19:29:45 -0300

    merge-main-deploy

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,6 +6,11 @@ import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import LandingPage from "./views/landing/LandingPage";
 import Home from "./views/home/Home";
+import Detail from "./views/detail/Detail";
+import Navbar from "./components/navbar/Navbar";
+import MangasSection from "./views/mangas-section/Mangas";
+import ComicsSection from "./views/comics-section/Comics";
+import FormCreate from "./components/formCreate/FormCreate"
 
 const router = createHashRouter([
 	{
@@ -13,18 +18,43 @@ const router = createHashRouter([
 		element: <LandingPage />,
 	},
 	{
-		path: "/home",
-		element: <Home />,
+		path: "/app",
+		element: <Navbar />,
+		children: [
+			{
+				path: "/app/home",
+				element: <Home />,
+			},
+			{
+				path: "/app//mangas",
+				element: <MangasSection />,
+			},
+			{
+				path: "/app//comics",
+				element: <ComicsSection />,
+			},
+			{
+				path: "/app//comic/:id",
+				element: <Detail />,
+			},
+			{
+				path: "/app//create",
+				element: <FormCreate />,
+			},
+			{
+				path: "/app/*",
+				element: <div>404</div>,
+			},
+		],
+		
 	},
-	// {
-	// 	path: "/detail/:param",
-	// 	element: <Detail />,
-	// },
-	// {
-	// 	path: "/add",
-	// 	element: <AddForm />,
-	// }
+	{
+		path: "/*",
+		element: <div>404</div>,
+	},
+
 ]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
