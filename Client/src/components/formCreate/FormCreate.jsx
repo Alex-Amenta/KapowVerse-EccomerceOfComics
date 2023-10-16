5ebd5135aebe6a14de024e738dbd755f46ae1bb2
Alex-Amenta <thekinggcat@gmail.com>
2023-10-16 00:57:07 -0300

    a

--- Client/src/components/formCreate/FormCreate.jsx
+++ Client/src/components/formCreate/FormCreate.jsx
@@ -6,42 +6,60 @@ import { createComic } from "../../redux/features/comicSlice";
 import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
 
+const initialFormData = {
+  title: "",
+  description: "",
+  price: "",
+  category: "",
+  author: "",
+  image: "",
+  stock: "",
+  publisher: "",
+};
+
+const initialError = {
+  title: "",
+  description: "",
+  price: "",
+  category: "",
+  author: "",
+  image: "",
+  stock: "",
+  publisher: "",
+};
+
+const categories = [
+  "Superheroes",
+  "Science Fiction",
+  "Fantasy",
+  "Adventure",
+  "Action",
+  "Horror",
+  "Mystery",
+  "Comedy",
+  "Drama",
+  "Romance",
+  "Suspense",
+];
+
+const publishers = ["Marvel", "DC", "Manga"];
+
 const FormCreate = () => {
   const dispatch = useDispatch();
   const [imagePreview, setImagePreview] = useState("");
-
-  const categories = [
-    "Superheroes",
-    "Science Fiction",
-    "Fantasy",
-    "Adventure",
-    "Action",
-    "Horror",
-    "Mystery",
-    "Comedy",
-    "Drama",
-    "Romance",
-    "Suspense",
-  ];
-
-  const publishers = ["Marvel", "DC", "Manga"];
-
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    price: "",
-    category: "",
-    author: "",
-    image: "",
-    stock: "",
-    publisher: "",
-  });
+  const [formData, setFormData] = useState(initialFormData);
+  const [error, setError] = useState(initialError);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
+    });
+
+    setError({
+      ...error,
+      [name]: "",
     });
   };
 
@@ -73,34 +91,75 @@ const FormCreate = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const formDataToSend = new FormData();
-      formDataToSend.append("title", formData.title);
-      formDataToSend.append("description", formData.description);
-      formDataToSend.append("price", formData.price);
-      formDataToSend.append("category", formData.category);
-      formDataToSend.append("author", formData.author);
-      formDataToSend.append("image", formData.image);
-      formDataToSend.append("stock", formData.stock);
-      formDataToSend.append("publisher", formData.publisher);
+    // ############### VALIDACIONES ###############
+    let hasError = false;
+    const newErrors = { ...initialError };
 
-      await axios.post("http://localhost:3001/comic", formDataToSend);
-      toast.success("Comic created successfully!", {
-        position: "bottom-center",
-      });
-      setFormData({
-        title: "",
-        description: "",
-        price: "",
-        category: "",
-        author: "",
-        image: "",
-        stock: "",
-        publisher: "",
-      });
-      setImagePreview("");
-    } catch (error) {
-      console.error(error);
+    // Validación del título
+    if (formData.title.length < 3) {
+      newErrors.title = "Title must be at least 3 characters long";
+      hasError = true;
+    }
+
+    // Validación de la descripción
+    if (formData.description.length < 5) {
+      newErrors.description = "Description must be at least 5 characters long";
+      hasError = true;
+    }
+
+    // Validación del precio
+    if (isNaN(formData.price) || parseFloat(formData.price) <= 0) {
+      newErrors.price = "Price must be a valid positive number";
+      hasError = true;
+    }
+
+    // Validación de la categoría
+    if (!categories.includes(formData.category)) {
+      newErrors.category = "Please select a category";
+      hasError = true;
+    }
+
+    // Validación del autor
+    if (formData.author.length < 3) {
+      newErrors.author = "Author name must be at least 3 characters long";
+      hasError = true;
+    }
+
+    // Validación del stock
+    if (isNaN(formData.stock) || parseInt(formData.stock) < 0) {
+      newErrors.stock = "Stock must be a valid non-negative number";
+      hasError = true;
+    }
+
+    // Validación del editor (publisher)
+    if (!publishers.includes(formData.publisher)) {
+      newErrors.publisher = "Please select a publisher";
+      hasError = true;
+    }
+
+    if (hasError) {
+      setError(newErrors);
+    } else {
+      try {
+        const formDataToSend = new FormData();
+        formDataToSend.append("title", formData.title);
+        formDataToSend.append("description", formData.description);
+        formDataToSend.append("price", formData.price);
+        formDataToSend.append("category", formData.category);
+        formDataToSend.append("author", formData.author);
+        formDataToSend.append("image", formData.image);
+        formDataToSend.append("stock", formData.stock);
+        formDataToSend.append("publisher", formData.publisher);
+
+        await axios.post("http://localhost:3001/comic", formDataToSend);
+        toast.success("Comic created successfully!", {
+          position: "bottom-center",
+        });
+        setFormData(initialFormData);
+        setImagePreview("");
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -109,71 +168,225 @@ const FormCreate = () => {
       <h2>Create new Comic</h2> <hr />
       <form onSubmit={handleSubmit} className={styles.formulario}>
         <div className={styles.inputContainer}>
-          <input
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            placeholder="Title..."
-          />
-          <input
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            placeholder="Description..."
-          />
-          <input
-            type="number"
-            name="price"
-            value={formData.price}
-            onChange={handleChange}
-            placeholder="Price..."
-          />
-          <select
-            name="category"
-            value={formData.category}
-            onChange={handleChange}
-            placeholder="Category..."
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
+          <div className={styles.input__group}>
+          <label
+						htmlFor="title"
+						className={styles.label}>
+						Title <b>*</b>
+					</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="Title..."
+              className={`${styles.input} ${
+                error.title ? styles["input-error"] : ""
+              }`}
+            />
+            <span className={styles.tooltiptext}>
+            &#128680; At least 3 characters long
+            </span>
+            {error.title && (
+              <span
+                onClick={() => setError({ ...error, title: "" })}
+                className={styles.tooltip}
+              >
+                {error.title}
+              </span>
+            )}
+          </div>
+
+          <div className={styles.input__group}>
+          <label
+						htmlFor="description"
+						className={styles.label}>
+						Description <b>*</b>
+					</label>
+            <textarea
+              name="description"
+              id="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Description..."
+              className={`${styles.input} ${
+                error.description ? styles["input-error"] : ""
+              }`}
+            />
+            <span className={styles.tooltiptext}>
+            &#128680; At least 5 characters long
+            </span>
+            {error.description && (
+              <span
+                onClick={() => setError({ ...error, description: "" })}
+                className={styles.tooltip}
+              >
+                {error.description}
+              </span>
+            )}
+          </div>
+          <div className={styles.input__group}>
+          <label
+						htmlFor="price"
+						className={styles.label}>
+						Price <b>*</b>
+					</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+              placeholder="Price..."
+              className={`${styles.input} ${
+                error.price ? styles["input-error"] : ""
+              }`}
+            />
+            <span className={styles.tooltiptext}>
+            &#128680; Price must be a valid positive number
+            </span>
+            {error.price && (
+              <span
+                onClick={() => setError({ ...error, price: "" })}
+                className={styles.tooltip}
+              >
+                {error.price}
+              </span>
+            )}
+          </div>
+          <div className={styles.input__group}>
+          <label
+						htmlFor="category"
+						className={styles.label}>
+						Category <b>*</b>
+					</label>
+            <select
+              name="category"
+              id="category"
+              value={formData.category}
+              onChange={handleChange}
+              placeholder="Category..."
+              className={`${styles.input} ${
+                error.category ? styles["input-error"] : ""
+              }`}
+            >
+              <option value="" disabled>
+                Select a category
               </option>
-            ))}
-          </select>
-          <input
-            type="text"
-            name="author"
-            value={formData.author}
-            onChange={handleChange}
-            placeholder="Author..."
-          />
-          <input
-            type="number"
-            name="stock"
-            value={formData.stock}
-            onChange={handleChange}
-            placeholder="Stock..."
-          />
-          <select
-            name="publisher"
-            value={formData.publisher}
-            onChange={handleChange}
-            placeholder="Publisher..."
-          >
-            <option value="" disabled>
-              Select a publisher
-            </option>
-            {publishers.map((publisher) => (
-              <option key={publisher} value={publisher}>
-                {publisher}
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <span className={styles.tooltiptext}>
+            &#128680; Please select a category
+            </span>
+            {error.category && (
+              <span
+                onClick={() => setError({ ...error, category: "" })}
+                className={styles.tooltip}
+              >
+                {error.category}
+              </span>
+            )}
+          </div>
+          <div className={styles.input__group}>
+          <label
+						htmlFor="author"
+						className={styles.label}>
+						Author <b>*</b>
+					</label>
+            <input
+              type="text"
+              name="author"
+              id="author"
+              value={formData.author}
+              onChange={handleChange}
+              placeholder="Author..."
+              className={`${styles.input} ${
+                error.author ? styles["input-error"] : ""
+              }`}
+            />
+            <span className={styles.tooltiptext}>
+            &#128680; At least 3 characters long
+            </span>
+            {error.author && (
+              <span
+                onClick={() => setError({ ...error, author: "" })}
+                className={styles.tooltip}
+              >
+                {error.author}
+              </span>
+            )}
+          </div>
+          <div className={styles.input__group}>
+          <label
+						htmlFor="stock"
+						className={styles.label}>
+						Stock <b>*</b>
+					</label>
+            <input
+              type="number"
+              name="stock"
+              id="stock"
+              value={formData.stock}
+              onChange={handleChange}
+              placeholder="Stock..."
+              className={`${styles.input} ${
+                error.stock ? styles["input-error"] : ""
+              }`}
+            />
+            <span className={styles.tooltiptext}>
+            &#128680; Stock must be a valid non-negative number
+            </span>
+            {error.stock && (
+              <span
+                onClick={() => setError({ ...error, stock: "" })}
+                className={styles.tooltip}
+              >
+                {error.stock}
+              </span>
+            )}
+          </div>
+          <div className={styles.input__group}>
+          <label
+						htmlFor="publisher"
+						className={styles.label}>
+						Publisher <b>*</b>
+					</label>
+            <select
+              name="publisher"
+              id="publisher"
+              value={formData.publisher}
+              onChange={handleChange}
+              placeholder="Publisher..."
+              className={`${styles.input} ${
+                error.publisher ? styles["input-error"] : ""
+              }`}
+            >
+              <option value="" disabled>
+                Select a publisher
               </option>
-            ))}
-          </select>
+              {publishers.map((publisher) => (
+                <option key={publisher} value={publisher}>
+                  {publisher}
+                </option>
+              ))}
+            </select>
+            <span className={styles.tooltiptext}>
+            &#128680; Please select a publisher
+            </span>
+            {error.publisher && (
+              <span
+                onClick={() => setError({ ...error, publisher: "" })}
+                className={styles.tooltip}
+              >
+                {error.publisher}
+              </span>
+            )}
+          </div>
         </div>
         <div className={styles.imageContainer}>
           <div className={styles.imagePreviewContainer}>
