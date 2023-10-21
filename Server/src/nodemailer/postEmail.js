301b9516794a9969078b9e2eba283676c534010f
AgusPostai <agustin.postai@gmail.com>
2023-10-21 17:20:23 -0300

    nodemailer actualizado

---
+++ Server/src/nodemailer/postEmail.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer");
+
+const { NODEMAILER_HOST, NODEMAILER_PORT, EMAIL_EMPRESA, PASSWORD_EMAIL } =
+  process.env;
+
+const transporter = nodemailer.createTransport({
+  host: NODEMAILER_HOST,
+  port: NODEMAILER_PORT,
+  secure: true,
+  auth: {
+    user: EMAIL_EMPRESA,
+    pass: PASSWORD_EMAIL,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+// console.log(transporter)
+module.exports = transporter;
