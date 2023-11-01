9b25f55dddeb72e73d1a85103ccc89cfc74d3365
AgusPostai <agustin.postai@gmail.com>
2023-11-01 19:14:37 -0300

    gmail lindo

--- Server/src/nodemailer/plantillaEmail.js
+++ Server/src/nodemailer/plantillaEmail.js
@@ -1,6 +1,12 @@
 const transporter = require("../nodemailer/postEmail");
-const logoOficial = require ("../../../Client/src/assets/logo-navbar.png")
-import naruto from "../../assets/naruto-alert.png";
+// const path = require("path");
+// require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+// const { FRONT_HOST, FE_DEPLOY, DEV} = process.env;
+// let ruta;
+// if (DEV && DEV === "development") 
+// 	ruta = FRONT_HOST; 
+// else ruta = FE_DEPLOY;
+
 const sendEmailConPlantilla = (to, template, data) => {
 
   let emailOptions;
@@ -11,127 +17,144 @@ const sendEmailConPlantilla = (to, template, data) => {
         to,
         subject: "Welcome to KapowVerse",
         html:
-          `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitinal//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-TRNASITIONAL.DTD">
-          <html XMLNS="http://www.w3.org/1999/html">
-            <head>
-              <meta http-equiv="Content-Type" content="text/html;charset= utf-8" />
-              <meta http-equiv="X-UA-Tompatible" content="IE=edge" />
-              <meta name="viewport" content="width=device-width, inital-scale=1.0" />
-
-                  <title>Bienvenido a nuestra página de comics</title>
-                  <style>
-                      body {
-                          background-color: #e8e8e8;
-                          font-family: "Bangers", sans-serif;
-                      }
-              
-                      table {
-                          width: 100%;
-                          border-collapse: collapse;
-                      }
-              
-                      table.container {
-                          width: 600px;
-                          margin: 0 auto;
-                          background-color: #fff;
-                          padding: 20px;
-                          border-radius: 5px;
-                          color: #212121;
-                      }
-              
-                      table.header {
-                          width: 100%;
-                      }
-              
-                      table.header img {
-                          max-width: 100px;
-                      }
-              
-                      table.header a {
-                          text-decoration: none;
-                          color: #212121;
-                      }
-              
-                      table.features {
-                          width: 100%;
-                      }
-              
-                      table.feature {
-                          width: 33.33%;
-                          text-align: center;
-                      }
-              
-                      .social-icons a {
-                          display: inline-block;
-                          margin-right: 10px;
-                      }
-              
-                      .social-icons img {
-                          max-width: 30px;
-                          vertical-align: middle;
-                      }
-              
-                      h1, h2 {
-                          font-weight: 1000;
-                          font-size: 17px;
-                      }
-                  </style>
-              </head>
-              <body>
-                  <table width="100%" bgcolor="#000">
-                      <tr>
-                          <td>
-                              <table class="container" align="center" bgcolor="#fff">
-                                  <tr>
-                                      <td>
-                                          <table class="header" width="100%">
-                                              <tr>
-                                                  <td style="width: 50%; text-align: left;">
-                                                      <img src="ruta_a_tu_logo.png" alt="Logo de la página">
-                                                  </td>
-                                                  <td style="width: 50%; text-align: right;" class="social-icons">
-                                                      <a href="enlace_a_instagram"><img src="ruta_a_icono_instagram.png" alt="Instagram"></a>
-                                                      <a href="enlace_a_twitter"><img src="ruta_a_icono_twitter.png" alt="Twitter"></a>
-                                                      <a href="enlace_a_facebook"><img src="ruta_a_icono_facebook.png" alt="Facebook"></a>
-                                                  </td>
-                                              </tr>
-                                          </table>
-                                          <h1>¡WELCOME TO KAPOWVERSE!</h1>
-                                          <p>We are excited to have you as part of our community. Here, you will be able to enjoy the best comics.</p>
-                                          <p>Thank you for joining us. We hope you have a great time!</p>
-                                      </td>
-                                  </tr>
-                              </table>
-                          </td>
-                      </tr>
-              
-                      <tr>
-                          <td>
-                              <table class="container" align="center" bgcolor="#fff">
-                                  <tr>
-                                      <td class="feature">
-                                          <h2>SHOP ONLINE</h2>
-                                          <p>We created this page so you can enjoy the comic you love from the comfort of your home with just two simple clicks. </p>
-                                      </td>
-                                      <td class="feature">
-                                          <h2>REVIEWS</h2>
-                                          <p>We have a review system in place so you can learn about the opinions of other buyers before purchasing your comics.</p>
-                                      </td>
-                                      <td class="feature">
-                                          <h2>CHAT   24/7</h2>
-                                          <p>We are available 24 hours a day to answer your questions and clear any doubts you may have whenever you need it..</p>
-                                      </td>
-                                  </tr>
-                              </table>
-                          </td>
-                      </tr>
-                  </table>
-              </body>
-              </html>
+        `<!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <title>Welcome to KapowVerse</title>
+            <style>
+    
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 0;
+                }
+        
+                .container {
+                    width: 80%;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #fff;
+                    border-radius: 5px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                }
+        
+                .header {
+                    background-color: #9f2d1c;
+                    color: #fff;
+                    text-align: center;
+                    padding: 10px;
+                    border-top-left-radius: 5px;
+                    border-top-right-radius: 5px;
+                }
+        
+                .content {
+                    padding: 20px;
+                }
+        
+                .footer {
+                    text-align: center;
+                    margin-top: 20px;
+                    color: #777;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <h1>Welcome to KapowVerse</h1>
+                </div>
+                <div class="content">
+             <h1>Hi ${data.userName}! </h1><hr/>
+          <p>Welcome to our online store of comics, mangas and more!!! At our store we strive to offer you the best shopping experience possible, which is why we have created an easy-to-navigate website. Additionally, our customer service team is at your disposal to resolve any questions or concerns you may have.</p>
+          <p>Thank You for registering on KapowVerse! <br/>
+          Please copy the following code to activate your account: <br/>
+          <p>${data.activationToken}</p> <br/>
+          <a href="${ruta}/activate/${data.activationToken}">Alternatively click here to activate account</a></p>
+          <p>Thank you for choosing us! <br/>
+          if you have any questions, please email us at <b>kaopwverse@gmail.com</b></p>
+          <p><b>Sincerely, <br/>
+          Your KapowVerse Team</b></p>
+                </div>
+            </div>
+        </body>
+        </html>
+           `
+      };
+      break;
+    case "Resend":
+      emailOptions = {
+        from: "kapowverse@gmail.com",
+        to,
+        subject: "Welcome to KapowVerse",
+        html:
+        `<!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <title>Welcome to KapowVerse</title>
+            <style>
+    
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 0;
+                }
+        
+                .container {
+                    width: 80%;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #fff;
+                    border-radius: 5px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                }
+        
+                .header {
+                    background-color: #9f2d1c;
+                    color: #fff;
+                    text-align: center;
+                    padding: 10px;
+                    border-top-left-radius: 5px;
+                    border-top-right-radius: 5px;
+                }
+        
+                .content {
+                    padding: 20px;
+                }
+        
+                .footer {
+                    text-align: center;
+                    margin-top: 20px;
+                    color: #777;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <h1>Welcome to KapowVerse</h1>
+                </div>
+                <div class="content">
+                <h1> Hi ${data.userName}! </h1><hr/>
+                <p>Please click the link below to activate your account.</p>
+                (Or copy the following code): <br/>
+                <p>${data.activationToken}</p> <br/>
+                <a href="${ruta}/activate/${data.activationToken}">Activate!</a></p>
+                
+                <p>Thank you for choosing us! <br/>
+                if you have any questions, please email us at <b>kaopwverse@gmail.com</b></p>
+                <p><b>Sincerely, <br/>
+                Your KapowVerse Team</b></p>
+                </div>
+            </div>
+        </body>
+        </html>
           `
       };
-
-
+      break;
   }
   transporter.sendMail(emailOptions, (error, info) => {
     if (error) {
